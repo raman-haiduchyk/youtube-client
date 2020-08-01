4e2fa72d043c183f22b6dfdaf4c9956cbbf04833
haid_roman <haidroman@gmail.com>
2020-08-01 16:01:12 +0300

    created necessary components
    maybe I'll add more components

--- youtube-client-app/src/app/app.module.ts
+++ youtube-client-app/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchListComponent } from './components/search-list/search-list.component';
+import { SearchItemComponent } from './components/search-item/search-item.component';
+import { AuthorizationFormComponent } from './components/authorization-form/authorization-form.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchListComponent,
+    SearchItemComponent,
+    AuthorizationFormComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
